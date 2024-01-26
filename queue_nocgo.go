288dba730a29e86bc05da5f2ade612c142e4a57a
Robert Zimțea <robert@zimtea.com>
2024-01-26 21:44:46 +0200

    fix: removed exra PRAGMA

--- queue_nocgo.go
+++ queue_nocgo.go
@@ -16,16 +16,6 @@ func openDB(dsn string) (*sql.DB, error) {
         return nil, errors.Trace(err)
     }
 
-    _, err = db.Exec(`
-        PRAGMA busy_timeout       = 10000;
-        PRAGMA journal_mode       = WAL;
-        PRAGMA journal_size_limit = 200000000;
-        PRAGMA synchronous        = NORMAL;
-        PRAGMA foreign_keys       = ON;
-        PRAGMA temp_store         = MEMORY;
-        PRAGMA cache_size         = -16000;
-    `)
-
     if err != nil {
         return nil, errors.Trace(err)
     }
